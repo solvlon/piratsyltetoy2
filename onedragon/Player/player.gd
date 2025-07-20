d25fc4ef135ac3b7f7ef247278817e55dd9199fb
Thomas DJIAN <thomas.djian91@gmail.com>
2025-07-20 21:30:47 +0200

    Removed player script

--- onedragon/Player/player.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-@onready var target = $PlayerController/Sprite2D/WeaponAttach
-
-@export var SPEED = 10
-
-func _process(delta: float) -> void:
-	position = lerp(position, target.position, SPEED * delta)
