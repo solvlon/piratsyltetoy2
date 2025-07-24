f850fc09b49534a20c2b8b03dad65de67574560b
simen@bluesmokeelectronics.no <simen@bluesmokeelectronics.no>
2025-07-24 21:23:05 +0200

    imported art assets, added blue tilmap

--- onedragon/Player/player.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-#class_name Player
-#
-#@onready var player_controller: CharacterBody2D = $PlayerController
-#
-#func get_player_position()-> Vector2:
-	#return player_controller.global_position
