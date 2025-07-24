af51c2a9e0522e4f4f27f2fe8cf1076c85034378
simen@bluesmokeelectronics.no <simen@bluesmokeelectronics.no>
2025-07-24 22:57:44 +0200

    added animation to dragon and fireball

--- onedragon/Dragon/dragon.gd
+++ onedragon/Dragon/dragon.gd
@@ -23,9 +23,18 @@ var _spawn_position
 @onready var claw_attack_area: Area2D = %ClawAttackArea
 @onready var fire_particles: GPUParticles2D = %FireParticles
 @onready var animation_player: AnimationPlayer = %AnimationPlayer
-@onready var sprite_2d: Sprite2D = %Sprite2D
+#@onready var sprite_2d: Sprite2D = %Sprite2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 @onready var fire_pre_particles: GPUParticles2D = %FirePreParticles
 @onready var follow_area: Area2D = %FollowArea
+@onready var fireball_spawn_left: Node2D = %fireballSpawn_left
+@onready var fireball_spawn_right: Node2D = %fireballSpawn_right
+@onready var fireball_spawn_up: Node2D = %fireballSpawn_up
+@onready var fireball_spawn_down: Node2D = %fireballSpawn_down
+
+var _fliped = false
+var fireball_spawn : Node2D = fireball_spawn_left
 
 func _ready() -> void:
 	health = maxHealth
@@ -39,7 +48,6 @@ func _physics_process(_delta: float) -> void:
 	else:
 		navigation_agent_2d.target_position = _spawn_position
 	
-	
 	if not navigation_agent_2d.is_navigation_finished() and not _is_attacking:
 		velocity =  position.direction_to( navigation_agent_2d.get_next_path_position())* SPEED
 	else : 
@@ -48,6 +56,41 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide()
 
 
+func _process(delta: float) -> void:
+
+	if velocity:
+		if velocity.angle() > -PI/4  and velocity.angle() < PI/4:
+			animated_sprite_2d.play("walk_right")
+			
+			if  _fliped:
+				print ("right")
+				animated_sprite_2d.scale.x *= -1
+				_fliped = false
+			fireball_spawn = fireball_spawn_right
+			#rotation = 0
+		if velocity.angle() > PI/4  and velocity.angle() < 3*PI/4:
+			animated_sprite_2d.play("walk_down")
+			if  _fliped:
+				animated_sprite_2d.scale.x *=  -1
+				_fliped = false
+			fireball_spawn = fireball_spawn_down
+			#rotation = 0
+		if velocity.angle() > 3*PI/4  or velocity.angle() < -3*PI/4:
+			
+			animated_sprite_2d.play("walk_right")
+			if not _fliped:
+				print ("left")
+				animated_sprite_2d.scale.x *=  -1
+				_fliped = true
+			fireball_spawn = fireball_spawn_left
+			
+		if velocity.angle() < -PI/4  and velocity.angle() > -3*PI/4:
+			animated_sprite_2d.play("walk_up")
+			if  _fliped:
+				animated_sprite_2d.scale.x *=  -1
+				_fliped = false
+			fireball_spawn = fireball_spawn_up
+		print(_fliped)
 
 func _on_attack_cooldown_timeout():
 	
@@ -62,14 +105,16 @@ func _on_attack_cooldown_timeout():
 
 func _fire_attack():
 	_is_attacking = true
-	fire_pre_particles.emitting = true
-	await fire_pre_particles.finished
-	fire_particles.emitting = true
+	#fire_pre_particles.emitting = true
+	#await fire_pre_particles.finished
+	#fire_particles.emitting = true
 	var fireball : FireBall = FIREBALL.instantiate()
+	fireball.position = fireball_spawn.position
 	fireball.move_direction = position.direction_to(player.controller.global_position)
 	add_child(fireball)
 	fire_particles.rotation = position.direction_to(player.controller.global_position).angle()
-	await  fire_particles.finished
+	await  fireball.spawned
+	await  get_tree().create_timer(.5).timeout
 	_is_attacking = false
 
 func _tile_swipe_attack():
@@ -81,12 +126,11 @@ func _tile_swipe_attack():
 func _claw_attack():
 	_is_attacking = true
 	var tween = create_tween()
-	tween.tween_property(sprite_2d,"position",position.direction_to(player.global_position) * 20, 0.1) 
-	tween.tween_property(sprite_2d,"position",Vector2.ZERO, 0.4) 
+	tween.tween_property(animated_sprite_2d,"position",position.direction_to(player.global_position) * 20, 0.1) 
+	tween.tween_property(animated_sprite_2d,"position",Vector2.ZERO, 0.4) 
 	await  tween.finished
 	_is_attacking = false
 	
 func on_hit(hitpoints, force):
 	health -= hitpoints
-	print(health)
 	animation_player.play("Hit")
--- onedragon/Dragon/fireball.gd
+++ onedragon/Dragon/fireball.gd
@@ -1,18 +1,44 @@
 extends Area2D
 class_name FireBall
 
+signal  spawned
+
 @export var damages = 50
 @export var force = 500
 const SPEED = 300
 
+@onready var spawn_animated_sprite_2d: AnimatedSprite2D = %SpawnAnimatedSprite2D
+@onready var hit_animated_sprite_2d: AnimatedSprite2D = %HitAnimatedSprite2D
+@onready var sprite_2d: Sprite2D = %Sprite2D
+
 var move_direction : Vector2
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+var _moving = false
+func _ready() -> void:
+	spawn_animated_sprite_2d.play("default")
+	sprite_2d.hide()
+	spawn_animated_sprite_2d.show()
+	await spawn_animated_sprite_2d.animation_finished
+	_moving = true
+	sprite_2d.show()
+	spawn_animated_sprite_2d.hide()
+	spawned.emit()
+	
 func _process(delta: float) -> void:
-	position += move_direction*delta * SPEED
+	if _moving:
+		position += move_direction*delta * SPEED
 
 func _on_despawn_timer_timeout() -> void:
-	queue_free()
+	_despawn()
 
 func _on_body_enter(body) -> void:
-	body.player.on_hit(damages, move_direction * force)
+	if body is PlayerController: 
+		body.player.on_hit(damages, move_direction * force)
+		_despawn()
+
+func _despawn():
+	sprite_2d.hide()
+	hit_animated_sprite_2d.show()
+	hit_animated_sprite_2d.play("default")
+	await hit_animated_sprite_2d.animation_finished
 	queue_free()
