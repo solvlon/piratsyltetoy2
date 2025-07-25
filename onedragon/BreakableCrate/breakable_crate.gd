364a78f16d2b419ee068d4dfd2b8f0d34b296ed9
Thomas DJIAN <thomas.djian91@gmail.com>
2025-07-25 10:22:09 +0200

    added chest

--- onedragon/BreakableCrate/breakable_crate.gd
+++ onedragon/BreakableCrate/breakable_crate.gd
@@ -1,6 +1,7 @@
 extends RigidBody2D
 
 @export var objToSpawn: PackedScene
+@export var disableCollisionOnOpen = true
 
 @onready var normalImage = $Normal
 @onready var destroyedImage = $Destroyed
@@ -9,7 +10,8 @@ extends RigidBody2D
 func on_hit(power, force):
 	normalImage.visible = false
 	destroyedImage.visible = true
-	collisionShape.set_deferred("disabled", true)
+	if disableCollisionOnOpen:
+		collisionShape.set_deferred("disabled", true)
 	var obj = objToSpawn.instantiate()
 	add_child(obj)
 	obj.global_position = global_position
