37bfa82d751f2f916326d7bfc1f08de96972784b
Thomas DJIAN <thomas.djian91@gmail.com>
2025-07-30 20:33:08 +0200

    add menu

--- onedragon/menu_manager.gd
+++ onedragon/menu_manager.gd
@@ -1,12 +1,21 @@
 extends CanvasLayer
 
-@onready var player = $"../Player"
-@onready var dragon = $"../Dragon"
+var player
+var dragon
 @onready var winScreen = $Victory
 @onready var looseScreen = $Defeat
+@onready var helpScreen = $Help
+@onready var playButotn = $Menu/PlayButton
+@onready var helpButton = $Menu/HelpButton
 
 func _process(delta: float) -> void:
 	if player.health <= 0:
 		looseScreen.visible = true
 	elif dragon.health <= 0:
 		winScreen.visible = true
+
+func showHelp() -> void:
+	helpScreen.visible = true
+
+func hideHelp() -> void:
+	helpScreen.visible = false
